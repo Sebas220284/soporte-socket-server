9d7df50b3a18d813dc3b9cf9be22918e2abbab49
Sebastian Vazquez <sebastianvazquez928@gmail.com>
2023-05-08 16:28:35 -0600

    server soporte

---
+++ server.js
@@ -0,0 +1,31 @@
+const http=require('http')
+
+
+
+
+
+
+const server= http.createServer()
+
+port = process.env.PORT || 8000;
+const io=require('socket.io')(server,{
+    cors:{origin:'*'}
+})
+
+io.on('connection',(socket)=>{
+    console.log('se ha conectado un cliente')
+
+    socket.broadcast.emit('chat_msj',{
+        usuario:'INFO',
+        mensaje:'Se ha conectado un nuevo usuario'
+    })
+
+
+socket.on('chat_msj',(data)=>{
+  io.emit('chat_msj',data)
+})
+
+})
+
+server.listen(port)
+console.log('corriendo en el puerto '+port)
